fdbed385192f47ff05cdd3eb5308de8222ee1d19
Jeeiii <giacomo.corrias7@gmail.com>
2024-05-19 01:36:09 +0200

    refactor: make cli exec

--- tasks/cli.ts
+++ tasks/cli.ts
@@ -1,15 +1,20 @@
+#!/usr/bin/env node
+
 import { program } from "commander"
 import { exec } from "child_process"
 import { log } from "console";
 import { welcome } from "./shared";
 
 program
+  .name("@ocskiurity/n0tte")
+  .description("FHE-ML cli")
+  .version("1.0.0")
   .command("run-task <task>")
   .description("commands w/ hardhat tasks")
   .action((task) => {
 
     welcome()
-    
+
     exec(`npx hardhat ${task}`, (error, stdout, stderr) => {
       if (error) {
         console.error(`Error: ${error.message}`);
