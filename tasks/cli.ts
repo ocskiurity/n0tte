a48f5f97430926712e6feb8e52e607da4d7ffca7
Jeeiii <giacomo.corrias7@gmail.com>
2024-05-19 01:02:18 +0200

    chore: add missing welcome banner

--- tasks/cli.ts
+++ tasks/cli.ts
@@ -1,11 +1,15 @@
 import { program } from "commander"
 import { exec } from "child_process"
 import { log } from "console";
+import { welcome } from "./shared";
 
 program
   .command("run-task <task>")
   .description("commands w/ hardhat tasks")
   .action((task) => {
+
+    welcome()
+    
     exec(`npx hardhat ${task}`, (error, stdout, stderr) => {
       if (error) {
         console.error(`Error: ${error.message}`);
