28e667f909acaabac35e1d3bcf72a6f067e7f128
Jeeiii <giacomo.corrias7@gmail.com>
2024-05-19 09:59:40 +0200

    feat: add e2e combine all task

---
+++ tasks/all.ts
@@ -0,0 +1,16 @@
+import { MLP1L } from "../types";
+import { task } from "hardhat/config";
+import type { TaskArguments } from "hardhat/types";
+import { log } from "./shared";
+
+task("all").setAction(async function (
+    _taskArguments: TaskArguments,
+    hre,
+) {
+    await hre.run('set:bias');
+    await hre.run('set:weights');
+    await hre.run('set:quantized-data');
+    await hre.run('inference');
+
+    log("ok")
+  });
--- tasks/index.ts
+++ tasks/index.ts
@@ -2,3 +2,4 @@ export * from "./setBias";
 export * from "./setWeights";
 export * from "./setQuantizedData";
 export * from "./inference";
+export * from "./all";
